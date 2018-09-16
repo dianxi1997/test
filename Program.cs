52c5ee2a5c7f71d651d0cecd2012a9237f582536
dianxi1997 <31642941+dianxi1997@users.noreply.github.com>
2018-09-16 16:33:06 +0800

    第二次提交

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace test_app
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
